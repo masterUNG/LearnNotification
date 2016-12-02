90dadf8dfbc552ec3bb4d189779a08035d542af3
masterUNG <phrombutr@gmail.com>
2016-12-02 09:41:41 +0700

    Setup Ok

--- app/src/main/java/appewtc/masterung/learnnotification/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learnnotification/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -23,13 +25,21 @@ public class MainActivity extends Activity {
 
         // Build notification
         // Actions are just fake
+        Uri soundUri = RingtoneManager.getDefaultUri(Notification.DEFAULT_SOUND);
+
         Notification noti = new Notification.Builder(this)
                 .setContentTitle("New mail from " + "test@gmail.com")
-                .setContentText("Subject").setSmallIcon(R.drawable.danger)
+                .setContentText("Subject")
+                .setSmallIcon(R.drawable.danger)
+                .setSound(soundUri)
                 .setContentIntent(pIntent).build();
 //                .addAction(R.drawable.danger, "Call", pIntent)
 //                .addAction(R.drawable.danger, "More", pIntent)
 //                .addAction(R.drawable.danger, "And more", pIntent).build();
+
+
+
+
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         // hide the notification after its selected
         noti.flags |= Notification.FLAG_AUTO_CANCEL;
