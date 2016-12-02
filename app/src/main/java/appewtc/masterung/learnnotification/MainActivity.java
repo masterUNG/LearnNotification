1bfcbc4130a0351d49ac57bbc1ad59e3e9a690e5
masterUNG <phrombutr@gmail.com>
2016-12-02 09:21:13 +0700

    Initial commit

---
+++ app/src/main/java/appewtc/masterung/learnnotification/MainActivity.java
@@ -0,0 +1,40 @@
+package appewtc.masterung.learnnotification;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void createNotification(View view) {
+        // Prepare intent which is triggered if the
+        // notification is selected
+        Intent intent = new Intent(this, NotificationReceiverActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+        // Build notification
+        // Actions are just fake
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle("New mail from " + "test@gmail.com")
+                .setContentText("Subject").setSmallIcon(R.drawable.danger)
+                .setContentIntent(pIntent).build();
+//                .addAction(R.drawable.danger, "Call", pIntent)
+//                .addAction(R.drawable.danger, "More", pIntent)
+//                .addAction(R.drawable.danger, "And more", pIntent).build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // hide the notification after its selected
+        noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(0, noti);
+
+    }
+}
